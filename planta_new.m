8da24850d74f6f67750c7eaaa6c74ed7b04ae9f8
Harold2828 <harold2828@hotmail.com>
2021-06-16 06:05:02 -0500

    Agregando los demás archivos

--- planta_new.m
+++ planta_new.m
@@ -1,42 +1,66 @@
 function [panel,turbina,battery,diesel_gen,lco,potencia]=planta_new(clima,panel,turbina,inverter,battery,lco,potenciaRequeria,hora)
 %%
+%Ecuaciones usadas
+%@(d_aire,area_barrido,eficiencia_turbina,vel_viento,numero_turbina)(1/2.*d_aire.*area_barrido.*eficiencia_turbina.*(vel_viento.^3).*numero_turbina.*10^-3 );
+%Potencia producida por la turbina 
+%     (((10^-3).*eficiencia_turbina.*d_aire.*area_barrido.*(vel_viento.^3).*numero_turbina)./(2));
+
 p_Turbina=@(eficiencia_turbina,d_aire,area_barrido,vel_viento,numero_turbina)...
     (1/2.*d_aire.*area_barrido.*eficiencia_turbina.*(vel_viento.^3).*numero_turbina.*10^-3 );
 
-p_Panel=@(eficiencia_panel,area,irradiancia,numero_panel)...
+%Potencia producida por los paneles
+
+%@(eficiencia_panel,area,irradiancia,numero_panel)(irradiancia.*eficiencia_panel.*area.*numero_panel.*10^-3);
+ p_Panel=@(eficiencia_panel,area,irradiancia,numero_panel)...
      (irradiancia.*eficiencia_panel.*area.*numero_panel.*10^-3);
 
+%Potencia de salida Diesel
 p_Diesel=@(potencia_requerida,pot_renovable)(potencia_requerida-pot_renovable);
-
+%Costo nivelado de la energia
 lcoe=@(lcoSol,lcoViento,lcoBat,lcoDiesel,energy_sol,energy_wind,energy_bat,energy_diesel)((lcoSol+lcoViento+lcoBat+lcoDiesel)./eGen);
 lcoe_formula=@(lcoe_used,energy_used)(lcoe_used.*energy_used);
 %%
+%panel_run=panel.cantidad(panel.cantidad>0);
 panel_run=panel.cantidad;
+%turbina_run=turbina.cantidad(turbina.cantidad>0);
 turbina_run=turbina.cantidad;
+%Generacion de potencia
 pv_gen=p_Panel(panel.eficiencia(hora),panel.area,clima.irradiancia(hora),panel_run);
 tur_gen=p_Turbina(turbina.eficiencia,clima.densidadAire(hora),clima.velViento(hora),turbina_run,turbina.areaBarrido);
+%Energia renovable total
 renovable_gen=sum([pv_gen,tur_gen],2);
+%Sistemas que cargan baterias
 posGG=logical(renovable_gen>potenciaRequeria(hora));
 posUG=~posGG;
 %%
+%Para cargar
 if isempty(battery.SOCi(posGG))==0
+    %posMM_1=logical(battery.SOCi<battery.SOCMax);
     posMM_1=ones(length(battery.SOCi),1); %
     posMM=logical(posMM_1.*posGG);
     if isempty(battery.SOCi(posMM))==0
+        %Carga la bateria
         battery.SOCi(posMM)=bateria(inverter.eficiencia,battery.eficiencia,...
             battery.SOCi(posMM),battery.SOCMax,battery.SOCMin,battery.autoDescarga,renovable_gen(posMM),...
             potenciaRequeria(hora),"carga");
     end
 end
 %%
+%Para descargar
 if isempty(battery.SOCi(posUG))==0
+    %posMm_1=logical(battery.SOCi>battery.SOCMin);
     posMm_1=battery.SOCi>battery.SOCMin;
-    posMm=logical(posMm_1.*posUG); 
+    posMm=logical(posMm_1.*posUG); %Para las que necesiten energía
     if isempty(battery.SOCi(posMm))==0
         antes=battery.SOCi(posMm);
         battery.SOCi(posMm)=bateria(inverter.eficiencia,battery.eficiencia,battery.SOCi(posMm),battery.SOCMax,...
             battery.SOCMin,battery.autoDescarga,renovable_gen(posMm),potenciaRequeria(hora),"descarga");
+        %La perdida de energia
          battery.SOCL(posMm,hora)=antes-battery.SOCi(posMm);
+%          keep=battery.SOCL(posMm,hora);
+%          keep(keep<battery.SOCMin)=battery.SOCMin;
+%          battery.SOCL(posMm,hora)=keep;
+         %La energia que aporta a la energia renovable
         renovable_gen(posMm,:)=renovable_gen(posMm,:)+battery.SOCi(posMm);
     end
 end
@@ -44,6 +68,7 @@ end
 diesel_gen=p_Diesel(potenciaRequeria(hora),renovable_gen);
 diesel_gen(diesel_gen<0)=diesel_gen(diesel_gen<0).*0;
 energia_generada=renovable_gen+diesel_gen;
+%lco.total=lcoe(lco.sol,lco.viento,lco.bat,lco.diesel,energia_generada);
 potencia.panel=pv_gen;
 potencia.turbina=tur_gen;
 potencia.diesel=diesel_gen;
@@ -81,4 +106,6 @@ end
             eBateria=descarga(SOC,ind_auto,e_gen,e_demand,ef_inverter);
             eBateria(eBateria>SOCMin)=SOCMin; %Porque la descarga es negativa
     end
+    %eBateria(eBateria>SOCMax)=SOCMax;
+    
  end
